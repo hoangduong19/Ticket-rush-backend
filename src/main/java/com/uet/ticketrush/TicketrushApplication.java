7f0c48d529a02828a5cd6ede084ab48c92325414
hoangduong19 <hoangduong190105@gmail.com>
2026-04-07 09:14:55 +0700

    Initialize Spring Boot Repository

---
+++ src/main/java/com/uet/ticketrush/TicketrushApplication.java
@@ -0,0 +1,13 @@
+package com.uet.ticketrush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketrushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketrushApplication.class, args);
+	}
+
+}
