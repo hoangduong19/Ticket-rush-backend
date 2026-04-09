736c139b93f6d67aa3680e7353345d0c48c20bdf
hoangduong19 <hoangduong190105@gmail.com>
2026-04-09 13:51:03 +0700

    Delete some dirty comments

--- src/main/java/com/uet/ticketrush/config/SecurityConfig.java
+++ src/main/java/com/uet/ticketrush/config/SecurityConfig.java
@@ -50,7 +50,6 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        // "Mở cửa" cho đúng ông Frontend
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
