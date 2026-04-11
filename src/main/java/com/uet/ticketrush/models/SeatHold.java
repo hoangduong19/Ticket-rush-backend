8692a27c5f1cee819696d8719dab4abaa73016ee
hoangduong19 <hoangduong190105@gmail.com>
2026-04-11 16:14:03 +0700

    Adding domain method in Seat and SeatHold

--- src/main/java/com/uet/ticketrush/models/SeatHold.java
+++ src/main/java/com/uet/ticketrush/models/SeatHold.java
@@ -3,6 +3,9 @@ package com.uet.ticketrush.models;
 import com.uet.ticketrush.enums.HoldStatus;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
@@ -41,4 +44,37 @@ public class SeatHold {
             inverseJoinColumns = @JoinColumn(name = "seat_id")
     )
     private Set<Seat> seats = new HashSet<>();
+
+    public boolean isExpired() {
+        return LocalDateTime.now().isAfter(expiresAt);
+    }
+
+    public void complete() {
+        this.status = HoldStatus.Completed;
+    }
+
+    public void markAsExpired() {
+        this.status = HoldStatus.Expired;
+    }
+
+    public static SeatHold createPendingHold(User user, Event event, Set<Seat> seats, int expiryMinutes) {
+        return SeatHold.builder()
+                .user(user)
+                .event(event)
+                .seats(seats)
+                .status(HoldStatus.Active)
+                .expiresAt(LocalDateTime.now().plusMinutes(expiryMinutes))
+                .build();
+    }
+
+    public BigDecimal getTotalPrice() {
+        return seats.stream()
+                .map(Seat::getPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    public long getSecondsLeft() {
+        long seconds = Duration.between(LocalDateTime.now(), expiresAt).getSeconds();
+        return Math.max(0, seconds);
+    }
 }
